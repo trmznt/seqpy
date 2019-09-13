277a6fd642b0236db8288c7e5a3c6e683d9ab37a
Anto - alrescha <anto@eijkman.go.id>
2019-09-14 06:53:21 +0700

    add language level

--- seqpy/core/cfuncs/setup.py
+++ seqpy/core/cfuncs/setup.py
@@ -3,11 +3,11 @@ from Cython.Build import cythonize
 import numpy
 
 setup(name="fastdx"
-	, ext_modules=cythonize('fastdx.pyx')
+	, ext_modules=cythonize('fastdx.pyx', language_level=3)
 	, include_dirs=[numpy.get_include()]
 	,)
 setup(name="genoutils"
-	, ext_modules=cythonize('genoutils.pyx')
+	, ext_modules=cythonize('genoutils.pyx', language_level=3)
 	, include_dirs=[numpy.get_include()]
 	,)
 
