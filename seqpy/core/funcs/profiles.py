8e9ba05467f045695561e29597b7d2273dfb19c2
Anto - alrescha <anto@eijkman.go.id>
2019-12-05 19:48:24 +0700

    add more optional arguments

--- seqpy/core/funcs/profiles.py
+++ seqpy/core/funcs/profiles.py
@@ -46,14 +46,14 @@ class SeqProfile(object):
 
     def consensus(self, threshold = 0.5, ref=None):
         """ return a consensus sequence """
-    
+
         cons_seq = bytearray()
         cons = self.mat
         char_set = self.char_set
         for i in range(0, len(cons)):
             max_pos = cons[i].argmax()
             if cons[i, max_pos] < threshold:
-                c = char_set[max_pos] + 32 if 64 < char_set[max_pos] < 91 else char_set[max_pos] 
+                c = char_set[max_pos] + 32 if 64 < char_set[max_pos] < 91 else char_set[max_pos]
             else:
                 c = char_set[max_pos]
             if ref and ref[i] == c:
@@ -82,13 +82,13 @@ def seq_profile(char_set, ign_set, iupac_set, mseq, apriori=1e-10, normalize=Tru
         prof.update(mseq)
         if normalize:
             prof.normalize()
-        assert prof.mat != None
+        assert prof.mat is not None
     return prof
 
-def na_profile(mseq, apriori=1e-10, normalize=True):
-    return seq_profile(NA_SET, IGN_SET, NA_IUPAC, mseq, apriori, normalize)
+def na_profile(mseq, apriori=1e-10, normalize=True, additional_set = b'', additional_ignore = b''):
+    return seq_profile(NA_SET + additional_set, IGN_SET + additional_ignore, NA_IUPAC, mseq, apriori, normalize)
 
-def aa_profile(mseq, apriori=1e-10, normalize=True):
-    return seq_profile(AA_SET, IGN_SET, None, mseq, apriori, normalize)
+def aa_profile(mseq, apriori=1e-10, normalize=True, additional_set = b'', additional_ignore = b''):
+    return seq_profile(AA_SET + additional_set, IGN_SET + additional_ignore, None, mseq, apriori, normalize)
 
 
