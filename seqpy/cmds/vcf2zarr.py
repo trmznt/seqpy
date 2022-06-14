bd4f19e45332cd16b1ebcc679c04f422713e598d
anto @ aleph <trimarsanto@gmail.com>
2022-06-14 13:02:33 +0700

    add max_alt_alleles arg to cope with indels

--- seqpy/cmds/vcf2zarr.py
+++ seqpy/cmds/vcf2zarr.py
@@ -9,6 +9,7 @@ from seqpy.core.sgk.sgio import read_vcf, save_dataset
 def init_argparser():
     p = arg_parser("Convert VCF to ZARR datastore")
     p.add_argument('-o', '--outfile')
+    p.add_argument('--max_alt_alleles', type=int, default=3)
     p.add_argument('infile')
     return p
 
@@ -16,7 +17,7 @@ def init_argparser():
 def vcf2zarr(args):
 
     cerr(f'I: reading VCF {args.infile}')
-    ds = read_vcf(args.infile)
+    ds = read_vcf(args.infile, max_alt_alleles=args.max_alt_alleles)
     save_dataset(ds, args.outfile)
     cerr(f'I:: datastore written to {args.outfile}')
 
--- seqpy/core/sgk/sgio.py
+++ seqpy/core/sgk/sgio.py
@@ -21,23 +21,23 @@ def _get_zarr_storage(path, mode='r'):
             return path
 
 
-def vcf_to_zarr(vcffile, outpath, fields=['INFO/*', 'FORMAT/*']):
+def vcf_to_zarr(vcffile, outpath, fields=['INFO/*', 'FORMAT/*'], max_alt_alleles=3):
 
     cerr(f'INFO: converting from {vcffile} to {outpath}')
     store = _get_zarr_storage(outpath)
-    sgkit.io.vcf.vcf_to_zarr(vcffile, store, fields=fields)
+    sgkit.io.vcf.vcf_to_zarr(vcffile, store, fields=fields, max_alt_alleles=max_alt_alleles)
     if hasattr(store, 'close'):
         store.close()
 
 
-def load_dataset(inpath):
+def load_dataset(inpath, max_alt_alleles=3):
     """ load zarr dataset and reset the encoding in all variables
         so that dataset can be saved using save_dataset
     """
 
     # in case the inpath is still VCF, just use read_vcf
     if inpath.lower().endswith('.vcf') or inpath.lower().endswith('.vcf.gz'):
-        return read_vcf(inpath)
+        return read_vcf(inpath, max_alt_alleles=max_alt_alleles)
 
     cerr(f'INFO: loading dataset from {inpath}')
     store = _get_zarr_storage(inpath, 'r')
@@ -56,10 +56,10 @@ def save_dataset(datastore, outpath):
         store.close()
 
 
-def read_vcf(path, fields=['INFO/*', 'FORMAT/*']):
+def read_vcf(path, fields=['INFO/*', 'FORMAT/*'], max_alt_alleles=3):
     cerr(f'INFO: reading VCF from {path}')
     memstore = zarr.storage.MemoryStore()
-    sgkit.io.vcf.vcf_to_zarr(path, memstore, fields=fields)
+    sgkit.io.vcf.vcf_to_zarr(path, memstore, fields=fields, max_alt_alleles=max_alt_alleles)
     ds = sgkit.load_dataset(memstore)
     for v in ds.variables:
         ds[v].encoding.clear()
