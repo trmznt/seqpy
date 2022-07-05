6c56cdc8c8c29c0731caa50f5879b3a670611ef2
anto @ aleph <trimarsanto@gmail.com>
2022-07-05 17:39:12 +0700

    reformat to pep8 style

--- seqpy/cmds/__init__.py
+++ seqpy/cmds/__init__.py
@@ -1,21 +1,19 @@
 __copyright__ = '''
 seqpy/cmds/__init__.py - part of seqpy
 
-(c) 2006 - 2012 Hidayat Trimarsanto <anto@eijkman.go.id> / <trimarsanto@gmail.com>
+(c) 2006 - 2022 Hidayat Trimarsanto <anto@eijkman.go.id> / <trimarsanto@gmail.com>
 
 All right reserved.
 This software is licensed under GPL v3 or later version.
 Please read the README.txt of this software.
 '''
 
-import sys, os
 import argparse
 import importlib
 import seqpy
 
+
 def execute(args):
-    if len(args) < 1:
-        usage()
 
     command = args[0]
     M = importlib.import_module('seqpy.cmds.' + command)
@@ -36,12 +34,12 @@ def execute(args):
         M.main(args)
 
 
-def arg_parser( description = None ):
+def arg_parser(description=None):
 
-    parser = argparse.ArgumentParser( description = description )
+    parser = argparse.ArgumentParser(description=description)
 
-    parser.add_argument('--noattr', action='append_const', dest='io_opts', const = 'noattr',
-            help = 'do not read or write sequence attribute within fasta format')
+    parser.add_argument('--noattr', action='append_const', dest='io_opts', const='noattr',
+                        help='do not read or write sequence attribute within fasta format')
 
     return parser
 
