05611fae0e8d1825d1c852e70d7547145db17a79
anto @ aleph <trimarsanto@gmail.com>
2022-07-05 17:31:11 +0700

    add capability to convert multi vcfs

--- seqpy/cmds/vcf2zarr.py
+++ seqpy/cmds/vcf2zarr.py
@@ -10,16 +10,27 @@ def init_argparser():
     p = arg_parser("Convert VCF to ZARR datastore")
     p.add_argument('-o', '--outfile')
     p.add_argument('--max_alt_alleles', type=int, default=3)
-    p.add_argument('infile')
+    p.add_argument('infiles', nargs='+')
     return p
 
 
 def vcf2zarr(args):
 
-    cerr(f'I: reading VCF {args.infile}')
-    ds = read_vcf(args.infile, max_alt_alleles=args.max_alt_alleles)
-    save_dataset(ds, args.outfile)
-    cerr(f'I:: datastore written to {args.outfile}')
+    if len(args.infiles) > 1:
+        from sgkit.io.vcf import vcf_to_zarr
+        from dask.distributed import Client
+
+        client = Client(n_workers=16, threads_per_worker=1)
+        cerr('[Working in parallel]')
+        vcf_to_zarr(args.infiles, args.outfile, tempdir=args.outfile + '-temp',
+                    max_alt_alleles=args.max_alt_alleles)
+
+    else:
+        cerr(f'[Reading VCF {args.infiles[0]}]')
+        ds = read_vcf(args.infiles[0], max_alt_alleles=args.max_alt_alleles)
+        save_dataset(ds, args.outfile)
+
+    cerr(f'[Datastore written to {args.outfile}]')
 
 
 def main(args):
