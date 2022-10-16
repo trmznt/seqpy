cd8e735d9885c62ea6b6279ed99ac10b2bd11c91
anto @ aleph <trimarsanto@gmail.com>
2022-10-16 07:17:48 +0700

    reformat to pep8 style

--- seqpy/__init__.py
+++ seqpy/__init__.py
@@ -1,49 +1,61 @@
 __copyright__ = '''
 seqpy/__init__.py - part of seqpy
 
-(c) 2006 - 2012 Hidayat Trimarsanto <anto@eijkman.go.id> / <trimarsanto@gmail.com>
+(c) 2006 - 2022 Hidayat Trimarsanto <trimarsanto@gmail.com>
 
 All right reserved.
 This software is licensed under GPL v3 or later version.
 Please read the README.txt of this software.
 '''
 
-import sys, os
+import sys
+import os
 
 # prepare seqpy/extlib path to sys.path
-sys.path.append( os.path.join(os.path.split( os.path.split(__file__)[0] )[0], 'seqpy/extlib' ))
+sys.path.append(os.path.join(os.path.split(os.path.split(__file__)[0])[0], 'seqpy/extlib'))
 
-def _COUT( text ):
-    print( text, file=sys.stdout )
 
-def _CERR( text ):
-    print( text, file=sys.stderr, flush = True )
+def _COUT(text):
+    print(text, file=sys.stdout)
+
+
+def _CERR(text):
+    print(text, file=sys.stderr, flush=True)
+
 
 _cout = _COUT
 _cerr = _CERR
 
-def cout( text ):
-    _cout( text )
 
-def cerr( text ):
-    _cerr( text )
+def cout(text):
+    _cout(text)
 
-def cexit( text, err=1):
-    _cerr( text )
+
+def cerr(text):
+    _cerr(text)
+
+
+def cexit(text, err=1):
+    _cerr(text)
     sys.exit(err)
+
 
 def set_cout(func):
     global _cout
     _cout = func
 
+
 def set_cerr(func):
     global _cerr
     _cerr = func
+
 
 def gzopen(filename, options='rt'):
 
     if filename.endswith('.gz'):
         import gzip
-        return gzip.open( filename, options )
+        return gzip.open(filename, options)
     else:
-        return open( filename, options )
+        return open(filename, options)
+
+# EOF
