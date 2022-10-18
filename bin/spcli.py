bba993733e730acfcc0592eacd8dd48bf9e2cf04
anto @ aleph <trimarsanto@gmail.com>
2022-10-18 21:23:47 +0700

    add bash completion

--- bin/spcli.py
+++ bin/spcli.py
@@ -18,6 +18,7 @@ import sys
 import os
 import argparse
 import platform
+import pathlib
 
 # prepare path to seqpy
 sys.path.append(os.path.split(os.path.split(__file__)[0])[0])
@@ -27,7 +28,7 @@ import seqpy
 
 def greet():
     seqpy.cerr('spcli - seqpy command line interface')
-    seqpy.cerr('(C) 2011-2012 Hidayat Trimarsanto <trimarsanto@gmail.com>')
+    seqpy.cerr('(C) 2011-2022 Hidayat Trimarsanto <trimarsanto@gmail.com>')
     seqpy.cerr(f'Host: {platform.uname().node}')
 
 
@@ -39,6 +40,9 @@ def usage():
 
 
 def main():
+
+    if '_ARGCOMPLETE' in os.environ:
+        autocomplete()
 
     greet()
     if len(sys.argv) == 1:
@@ -59,10 +63,16 @@ def main():
                     init_argparser = _l['init_argparser']
                     p = init_argparser()
                     if not isinstance(p, argparse.ArgumentParser):
-                        seqpy.cerr('ERR: init_argparser() did not return ArgumentParser instance')
+                        seqpy.cerr(
+                            'ERR: init_argparser() did not return ArgumentParser instance')
                         sys.exit(1)
-                    p.add_argument('--debug', default=False, action='store_true',
-                                   help='open ipython3 pdb console when exception occurs')
+
+                    try:
+                        p.add_argument('--debug', default=False, action='store_true',
+                                       help='open ipython3 pdb console when exception occurs')
+                    except argparse.ArgumentError:
+                        pass
+
                     argp = p.parse_args(sys.argv[1:])
                     if argp.debug:
                         from ipdb import launch_ipdb_on_exception
@@ -86,6 +96,40 @@ def main():
     else:
         from seqpy import cmds
         cmds.execute(sys.argv[1:])
+
+
+def autocomplete():
+
+    # prepare line
+    line = os.environ.get('COMP_LINE', '')
+    tokens = line.split()
+    last_token = tokens[-1]
+
+    if len(tokens) > 1 and (last_token.startswith(',') or last_token.startswith('~')):
+        # let bash complete  use directory listing
+        sys.exit(1)
+
+    # prepare the completion lists
+    # read seqpy.cmds directory
+    import seqpy.cmds
+    cmds_directory = pathlib.Path(seqpy.cmds.__file__).parent
+    cmds = set(
+        [p.name.removesuffix('.py') for p in cmds_directory.iterdir()]
+    ) - {'__init__', '__pycache__'}
+
+    if len(tokens) == 1:
+        completions = sorted(cmds)
+
+    else:
+        completions = sorted(
+            [opt for opt in cmds if opt.startswith(last_token)]
+        )
+
+    # send results through fd 8
+    ifs = os.environ.get('IFS', '\013')
+    out_stream = os.fdopen(8, 'w')
+    out_stream.write(ifs.join(completions))
+    sys.exit(0)
 
 
 if __name__ == '__main__':
