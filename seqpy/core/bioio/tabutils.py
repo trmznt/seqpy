35428481e318dd6e1f5df5af5f351f3d68096ebb
anto @ aleph <trimarsanto@gmail.com>
2022-10-18 21:25:21 +0700

    add metadata utility as pandas extension

--- seqpy/core/bioio/tabutils.py
+++ seqpy/core/bioio/tabutils.py
@@ -1,24 +1,97 @@
 # tabutils.py
 
 import pandas as pd
+import pathlib
+
+from enum import Enum
+
+# tabular format for genomic data
+#
+# Sample-based Rows
+# =================
+#
+# headers: SAMPLE, CHR:POS, ...
+#
+# each row contains a single sample
+#
+# Variant-based Column
+#
+
+
+def read_file(infile):
+    match exts := pathlib.Path(infile).suffixes:
+        case [*_, '.feather']:
+            return pd.read_feather(infile)
+        case [*_, '.txt'] | [*_, '.tsv'] | [*_, '.tsv', '.gz']:
+            return pd.read_table(infile, sep='\t')
+        case [*_, '.csv'] | [*_, '.csv', '.gz']:
+            return pd.read_table(infile, sep=',')
+
+    raise RuntimeError(f'Unknown extension type: {exts}')
+
+
+def write_file(outfile, dataframe):
+    match pathlib.Path(outfile).suffixes:
+        case [*_, '.feather']:
+            return dataframe.to_feather(outfile)
+        case [*_, '.tsv'] | [*_, '.txt'] | [*_, '.tsv', '.gz'] | [*_, '.txt', '.gz']:
+            return dataframe.to_csv(outfile, sep='\t', index=False)
+        case [*_, '.csv'] | [*_, '.csv', '.gz']:
+            return dataframe.to_csv(outfile, sep=',', index=False)
+    raise RuntimeError('Unknown extension type')
+
+
+@pd.api.extensions.register_dataframe_accessor("meta")
+class MetaAccessor(object):
+
+    def __init__(self, df):
+        self._df = df
+
+    def join_to_samples(self, samples, columns):
+
+        # use pandas' join mechanism
+        for c in columns:
+            if c not in self._df:
+                raise ValueError(f'Column {c} does not exits')
+        sample_df = pd.DataFrame(dict(SAMPLE=samples))
+        return sample_df.join(self._df.loc[:, columns].set_index(columns[0]),
+                              on='SAMPLE', how='inner')
+
+    def join(self, right_df, on_column):
+
+        # use pandas' join mechanism
+        # import IPython; IPython.embed()
+        return self._df.join(right_df.set_index(on_column), on=on_column, how='inner')
+
+    def to_dict(self, key_column, value_column):
+        return dict(zip(self._df[key_column], self._df[value_column]))
+
+
+class FormatType(Enum):
+    SAMPLE = 1
+    VARIANT = 2
 
 
 @pd.api.extensions.register_dataframe_accessor("geno")
-class GenoAccessor:
+class GenoAccessor(object):
 
-    def __init__(self, pandas_obj):
-        self._validate(pandas_obj)
-        self._obj = pandas_obj
+    def __init__(self, df):
+        self._type = None
+        self._df = df
+        self._sampledf = None
+        self._metadf = None
+        self._validate_process(df)
 
-    @staticmethod
-    def _validate(obj):
-        if obj.columns[0] == 'SAMPLE':
-            # sample-based
-            self._type = 'sample'
+    def _validate_process(self, df):
+        if df.columns[0] == 'SAMPLE':
+            # sample-based rows
+            self._type = FormatType.SAMPLE
+            self._process_sample_format()
 
-        elif obj.columns[0] == 'CHROM' and obj.columns[1] == 'POS':
+        elif df.columns[0] == 'CHROM' and df.columns[1] == 'POS':
             # position-based
-            self._type = 'position'
+            self._type = FormatType.VARIANT
+            self._process_variant_format()
 
         else:
             raise AttributeError('This dataframe does not conform geno specs')
@@ -28,7 +101,7 @@ class GenoAccessor:
     def to_sampletype(self):
 
         # check that we start from position
-        if self._type != 'position':
+        if self._type != 'variant':
             raise ValueError('Dataframe is already position-based')
 
     def to_positiontype(self):
@@ -51,7 +124,44 @@ class GenoAccessor:
     def get_positionids(self):
         pass
 
-    def get_allele_dataframe(self):
+    def get_alleles(self):
+        return self._alleledf
+
+    def get_samples(self):
+        return self._sampledf['SAMPLE']
+
+    def _process_sample_format(self):
+        # create new dataframe without metadata row but still with metadata columns
+        metarows = self._df['SAMPLE'].str.startswith('%')
+        allelecolumns = self._df.columns.str.contains(':')
+        self._sampledf = self._df[~metarows]
+        self._metarows = self._df[metarows]
+        self._metacolumns = None
+        self._alleledf = self._sampledf.loc[:, allelecolumns]
+
+    def _process_variant_format(self):
+        # create new dataframe without metadata columns
+        raise NotImplementedError("the functionality is still being worked on")
+        metacolumns = self._df.columns.str.startswith('%')
+        self._metacolumns = self._df.loc[:, metacolumns]
+        samplecolumns = self._df.columns[~metacolumns]
+
+    def to_mhaps(self, mhap_specs):
+        """ return a new DF containing microhaplotype strings """
+        pass
+
+
+@pd.api.extensions.register_dataframe_accessor("mhap")
+class MicroHapAccessor(object):
+
+    def __init__(self, df):
+        self._df = df
+
+    def to_numerics(self):
+        """ return a new DF containing microhaplotype numerics """
+        # for each mhap:
+        #   perform value_count(), sort and convert to dict
+        #   create new series based on the dict
         pass
 
 
