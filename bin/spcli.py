2f728f69b476cc9b846da98be5a928df56830bad
anto @ alrescha <trimarsanto@gmail.com>
2024-03-28 15:25:35 +0930

    feat: use subcommands library

--- bin/spcli.py
+++ bin/spcli.py
@@ -1,143 +1,28 @@
 #!/usr/bin/env python3
 # PYTHON_ARGCOMPLETE_OK
 
-__copyright__ = '''
-spcli - part of seqpy
+__copyright__ = '(c) 2024, Hidayat Trimarsanto <trimarsanto@gmail.com'
+__license__ = 'MIT'
+__version__ = '2024.03.03'
 
-(c) 2011-2022 Hidayat Trimarsanto <trimarsanto@gmail.com>
-
-All right reserved.
-This software is licensed under GPL v3 or later version.
-Please read the README.txt of this software.
-'''
-
-##
-# spcli.py - seqpy command line interface
-##
 
 import sys
-import os
-import argparse
-import platform
-import argcomplete
 import pathlib
 
-# prepare path to seqpy
-sys.path.append(os.path.split(os.path.split(__file__)[0])[0])
+sys.path.append(pathlib.Path(__file__).parent.parent.as_posix())
 
-import seqpy
-
-
-def greet():
-    seqpy.cerr('spcli - seqpy command line interface', stamp=False)
-    seqpy.cerr('(C) 2011-2022 Hidayat Trimarsanto <trimarsanto@gmail.com>', stamp=False)
-    seqpy.cerr(f'Host: {platform.uname().node}', stamp=False)
-
-
-def usage():
-    seqpy.cerr('  usage:', stamp=False)
-    seqpy.cerr('    spcli scriptfile/CMD [ARGS]', stamp=False)
-    seqpy.cerr('  try: spcli showcmds', stamp=False)
-    sys.exit(0)
+from seqpy import subcommands
 
 
 def main():
 
-    tokens = []
-    if '_ARGCOMPLETE' in os.environ:
-        line = os.environ.get('COMP_LINE', '')
-        tokens = line.split()
-        if len(tokens) == 1 or (len(tokens) == 2 and not line.endswith(' ')):
-            autocomplete(tokens)
-        os.environ['COMP_LINE'] = line.split(' ', 1)[1]
-        os.environ['COMP_POINT'] = str(len(os.environ['COMP_LINE']))
-        cmd = tokens[1]
-    else:
-        greet()
-        if len(sys.argv) == 1:
-            usage()
-        cmd = sys.argv[1]
+    cmds = subcommands.SubCommands(
+        modules=['seqpy.cmds'],
+        allow_any_script=True,
+        allow_shell=True
+    )
 
-    if cmd.endswith('.py'):
-        # will execute a script file
-        seqpy.cerr(f'Attempting to run script: {cmd}')
-        if cmd.startswith('~'):
-            cmd = pathlib.Path(cmd).expanduser()
-        with open(cmd) as fh:
-            code = compile(fh.read(), cmd, 'exec')
-            _l = {'__name__': '__spcli_main__'}
-            exec(code, None, _l)
-            if 'main' in _l:
-                globals().update(_l)
-                main = _l['main']
-                if 'init_argparser' in _l:
-                    init_argparser = _l['init_argparser']
-                    p = init_argparser()
-                    if not isinstance(p, argparse.ArgumentParser):
-                        seqpy.cerr(
-                            'ERR: init_argparser() did not return ArgumentParser instance')
-                        sys.exit(1)
-
-                    try:
-                        p.add_argument('--debug', default=False, action='store_true',
-                                       help='open ipython3 pdb console when exception occurs')
-                    except argparse.ArgumentError:
-                        pass
-
-                    argcomplete.autocomplete(p)
-                    argp = p.parse_args(sys.argv[2:])
-                    if argp.debug:
-                        from ipdb import launch_ipdb_on_exception
-                        with launch_ipdb_on_exception():
-                            seqpy.cerr('WARN: running in debug mode')
-                            main(argp)
-                    else:
-                        main(argp)
-                else:
-                    import inspect
-                    if 'args' in inspect.signature(main).parameters:
-                        main(args=sys.argv)
-                    else:
-                        main()
-
-    elif cmd == '-i':
-        # interactive
-        import IPython
-        IPython.embed()
-
-    else:
-        from seqpy import cmds
-        cmds.execute(tokens[1:] if any(tokens) else sys.argv[1:])
-
-
-def autocomplete(tokens):
-
-    from seqpy.cmds import list_commands
-
-    # prepare line
-
-    last_token = tokens[-1]
-
-    if len(tokens) > 1 and (last_token.startswith('.') or last_token.startswith('~')):
-        # let bash complete  use directory listing
-        sys.exit(1)
-
-    # prepare the completion lists
-    cmds = list_commands()
-
-    if len(tokens) == 1:
-        completions = sorted(cmds)
-
-    else:
-        completions = sorted(
-            [opt for opt in cmds if opt.startswith(last_token)]
-        )
-
-    # send results through fd 8
-    ifs = os.environ.get('IFS', '\013')
-    out_stream = os.fdopen(8, 'w')
-    out_stream.write(ifs.join(completions))
-    sys.exit(0)
+    cmds.main()
 
 
 if __name__ == '__main__':
---
+++ seqpy/subcommands.py
@@ -0,0 +1,277 @@
+# subcommands.py
+# [https://github.com/trmznt/subcommands]
+
+__copyright__ = "(c) 2024, Hidayat Trimarsanto <trimarsanto@gmail.com>"
+__license__ = "MIT"
+__version__ = '2024.03.10'
+
+# this module provides subcommands, eg. PROG subcommand [options]
+
+import os
+import sys
+import platform
+import pathlib
+import importlib
+import argparse
+import argcomplete
+from typing import Callable
+
+
+def _cout(msg: str):
+    print(msg, file=sys.stdout)
+
+
+def _cerr(msg: str):
+    print(msg, file=sys.stderr)
+    sys.stderr.flush()
+
+
+def _cexit(msg: str, exit_code: int = 1):
+    _cerr(msg)
+    sys.exit(exit_code)
+
+
+def arg_parser(desc: str = ''):
+    p = argparse.ArgumentParser(description=desc)
+    return add_debug_to_parser(p)
+
+
+def add_debug_to_parser(p: argparse.ArgumentParser):
+
+    try:
+        p.add_argument('--debug', action='store_true', default=False,
+                       help='open ipdb when uncatched exception is raised')
+    except argparse.ArgumentError:
+        pass
+
+    return p
+
+
+class SubCommands(object):
+
+    def __init__(
+        self,
+
+        # list of Python module names to check for commands, eg:
+        # ['ngs_pipeline.cmds'] or ['seqpy.mds']
+        # the order implies precedence
+        modules: list[str] = [],
+
+        # environment variable name to be used to get list of Python modules
+        # to check for commands, the order implies precedence
+        module_env: str | None = None,
+
+        # whether mdules named in env variable takes precedence
+        env_takes_precedence: bool = False,
+
+        # set true to allow running scripts located in any directory
+        allow_any_script: bool = False,
+
+        # set true to allow for IPython shell
+        allow_shell: bool = False,
+
+        # set for specific greet, usage and help functions
+        greet_func: Callable | None = None,
+        usage_func: Callable | None = None,
+        help_func: Callable | None = None
+    ):
+
+        self.allow_any_script = allow_any_script
+        self.allow_shell = allow_shell
+        self.greet = greet_func or self.generic_greet
+        self.usage = usage_func or self.generic_usage
+        self.help = help_func or self.generic_help
+        self.prog_name = sys.argv[0].rsplit('/', 1)[-1]
+
+        # set up module list
+        self.modules = modules
+        if module_env:
+            module_envs = [m for m in os.environ.get(module_env, '').split(':')
+                           if m]
+            if env_takes_precedence:
+                self.modules = module_envs + self.modules
+            else:
+                self.modules += module_envs
+
+    def autocomplete(self, tokens: list[str]):
+
+        last_token = tokens[-1]
+
+        # prepare the completion lists
+        cmds = self.get_command_list()
+
+        if len(tokens) == 1:
+            completions = sorted(cmds)
+
+        else:
+            completions = sorted(
+                [opt for opt in cmds if opt.startswith(last_token)]
+            )
+
+        # send results through fd 8
+        ifs = os.environ.get('IFS', '\013')
+        out_stream = os.fdopen(8, 'w')
+        out_stream.write(ifs.join(completions))
+        sys.exit(0)
+
+    def generic_usage(self):
+        _cexit(
+            f'  usage:\n'
+            f'      {self.prog_name} CMD [OPTIONS]\n'
+            f'      {self.prog_name} [-i] [-l] [-h]\n'
+            f'  try: {self.prog_name} -l'
+        )
+
+    def generic_greet(self):
+        _cerr(f'{self.prog_name} - command line interface')
+        _cerr(f'Host: {platform.uname().node}')
+
+    def generic_help(self):
+        self.generic_usage()
+
+    def get_command_list(self) -> list[str]:
+        # read sqpy.cmds directory
+        cmd_files = []
+        for module in self.modules:
+            M = importlib.import_module(module)
+            cmd_directory = pathlib.Path(M.__path__[0])
+            cmd_files += cmd_directory.iterdir()
+        cmds = set(
+            [p.name.removesuffix('.py').replace('_', '-') for p in cmd_files]
+        ) - {'--init--', '--pycache--'}
+        return sorted(cmds)
+
+    def show_commands(self):
+        _cout('Available commands:')
+        for cmd in self.get_command_list():
+            _cout(f'  {cmd}')
+        sys.exit(0)
+
+    def run_main(
+        self,
+        main: Callable | None,
+        init_argparser: Callable | None,
+        args: list[str]
+    ):
+
+        if init_argparser is not None:
+            parser = init_argparser()
+            if not isinstance(parser, argparse.ArgumentParser):
+                _cexit('ERR: init_argparser() did not return ArgumentParser '
+                       'instance')
+            add_debug_to_parser(parser)
+
+            # perform bash autocompletion if needed
+            argcomplete.autocomplete(parser)
+
+            # if autocomplete does not exit:
+            args = parser.parse_args(args[1:])
+
+            if main is not None:
+                if args.debug:
+                    from ipdb import launch_ipdb_on_exception
+                    with launch_ipdb_on_exception():
+                        _cerr('WARN: running in debug mode')
+                        main(args)
+                else:
+                    main(args)
+            else:
+                _cexit('ERR: init_argparser() exists but main() does not')
+
+        elif main is not None:
+            import inspect
+            if 'args' in inspect.signature(main).parameters:
+                main(args=args)
+            else:
+                main()
+
+        # do nothing here
+
+    def run_cmd(self, args: list[str]):
+        """ this method will run module init_argparser() and main() """
+
+        command = args[0].replace('-', '_')
+        for module in self.modules:
+            try:
+                module_name = f'{module}.{command}'
+                M = importlib.import_module(module_name)
+                break
+            except ModuleNotFoundError as err:
+                if err.name != module_name:
+                    raise
+        else:
+            _cexit(f'Command: {args[0]} does not exist! '
+                   f'Please check with "{self.prog_name} -l".')
+
+        _cerr(f'Executing: {M.__file__}')
+        self.run_main(
+            getattr(M, 'main', None),
+            getattr(M, 'init_argparser', None),
+            args
+        )
+
+    def run_script(self, args: list[str]):
+        """ this method will run an arbitrary python script """
+
+        path = args[0]
+        _cerr(f'Executing: {path}')
+
+        # expand home directory
+        if path.startswith('~'):
+            path = pathlib.Path(path).expanduser()
+
+        with open(path) as fh:
+            code = compile(fh.read(), path, 'exec')
+            _l = {'__name__': '__anyscript_main__'}
+            exec(code, globals(), _l)
+            self.run_main(
+                _l.get('main', None),
+                _l.get('init_argparser', None),
+                args
+            )
+
+    def main(self):
+
+        tokens = []
+
+        if '_ARGCOMPLETE' in os.environ:
+            # we are invoked from argcomplete
+            line = os.environ.get('COMP_LINE', '')
+            tokens = line.split()
+            if len(tokens) == 1 or (len(tokens) == 2 and
+                                    not line.endswith(' ')):
+                self.autocomplete(tokens)
+            os.environ['COMP_LINE'] = line.split(' ', 1)[1]
+            os.environ['COMP_POINT'] = str(len(os.environ['COMP_LINE']))
+            cmd = tokens[1]
+        else:
+            self.greet()
+            if len(sys.argv) == 1:
+                self.usage()
+            cmd = sys.argv[1]
+
+        if cmd == '-i' and self.allow_shell:
+            # go to interactive mode
+            import IPython
+            IPython.embed()
+
+        if cmd == '-l':
+            # show available commands
+            self.show_commands()
+
+        elif cmd == '-h':
+            # show some help
+            self.help()
+
+        elif cmd.endswith('.py') and self.allow_any_script:
+
+            # run script, with:
+            #   path = cmd
+            #   args = sys.argv[2:] (eg. 'my_prog my_script arg0 arg1')
+            self.run_script(tokens[1:] if any(tokens) else sys.argv[1:])
+
+        else:
+            self.run_cmd(tokens[1:] if any(tokens) else sys.argv[1:])
+
+
+# EOF
